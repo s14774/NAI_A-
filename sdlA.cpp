5de940334aa6354fbc15456396769cc23741c9c3
Nezumi Ryu <NezumiRyu@gmail.com>
2018-11-05 13:14:35 +0100

    halfway to add csv files compability

--- sdlA.cpp
+++ sdlA.cpp
@@ -6,37 +6,189 @@
 #define windowSizeX 1000
 #define windowSizeY 1000
 
-#define fieldX 10
-#define fieldY 10
-
 #define distanceBetweenCells 3
 
-// <x, y>
-typedef std::pair<int, int> Point;
-// <f, <x, y>>
-// f = g + h
-// g(Point) = distance(start, Point)
-// h(Point) = expected distance (Point, end)
-typedef std::pair<double, std::pair<int, int>> fPoint; 
+// // <x, y>
+// typedef std::pair<int, int> Point;
+// // <f, <x, y>>
+// // f = g + h
+// // g(Point) = distance(start, Point)
+// // h(Point) = expected distance (Point, end)
+// typedef std::pair<double, std::pair<int, int>> fPoint; 
 
-bool isPointInsideGrid(int x, int y){
+struct Point {
+  Point() : x(-1), y(-1), 
+  f(FLT_MAX), g(FLT_MAX), h(FLT_MAX), 
+  parentX(-1), parentY(-1), status(0) {}
+  
+  int x, y;
+  // f = g + h
+  // g(Point) = distance(start, Point)
+  // h(Point) = expected distance (Point, end)
+  double f, g, h;
+  int parentX, parentY;
+  // 0 = empty space
+  // 1 = wall
+  // 2 = visited
+  // 3 = start
+  // 4 = end
+  int status;
+};
+
+bool isPointInsideGrid(int x, int y, int fieldX, int fieldY){
   return (x >= 0) && (x < fieldX) && (y >= 0) && (y < fieldY);
 }
 
-bool isPointInsideGrid(std::pair<int, int> p){
-  return (p.first >= 0) && (p.first < fieldX) && (p.second >= 0) && (p.second < fieldY)
+// bool isPointInsideGrid(std::pair<int, int> p){
+//   return (p.first >= 0) && (p.first < fieldX) && (p.second >= 0) && (p.second < fieldY);
+// }
+
+bool isPointInsideGrid(Point p, int fieldX, int fieldY){
+  return (p.x >= 0) && (p.x < fieldX) && (p.y >= 0) && (p.y < fieldY);
 }
 
-bool isPointNotBlocked(int grid[fieldX][fieldY], int x, int y){
+bool isPointNotBlocked(int **grid, int x, int y){
   return (grid[x][y] == 0);
 }
 
-bool isPointDestination(){
+bool isPointDestination(){}
+
+void aStarSearchNextMove(Point p, Point destination, int fieldX, int fieldY){
+  if(!isPointInsideGrid(p, fieldX, fieldY)){
+    printf("ERR: Point is outside field!");
+    return;
+  }
+
 
 }
 
-int main()
+double getHParam(int x, int y, Point p){
+  return ((double)sqrt((x-p.x)*(x-p.x)+(y-p.y)*(y-p.y)));
+}
+
+int countCharQuantity(std::string s, char c){
+  char * charstring = new char [s.length()+1];
+  strcpy(charstring, s.c_str());
+
+  int quantity = 0;
+  for(int i=0;i<s.length();i++){
+    if(charstring[i] == c){
+      quantity++;
+    }
+  }
+  return quantity;
+}
+
+int char2int(char c){
+  return(c-'0');
+}
+
+void LineToIntArray(std::string s, int* array){
+  char * charstring = new char [s.length()+1];
+  strcpy(charstring, s.c_str());
+  int arrayIterator = 0;
+  int tempValu = 0;
+  for(int i=0;i<s.length();i++){
+    if(charstring[i]>='0' && charstring[i]<='9'){
+      tempValu=tempValu*10+char2int(charstring[i]);
+    }
+    if(charstring[i]==','){
+      array[arrayIterator++]=tempValu;
+      tempValu=0;
+    }
+  }
+  array[arrayIterator]=tempValu;
+}
+
+int main(int argc, char* argv[])
 {
+  if (argc != 2) {
+    printf("Usage: %s filename.csv\n",argv[0]);
+    return 1;
+  }
+
+  std::string line;
+  std::ifstream file (argv[1]);
+
+  if (! file.is_open()){
+    printf("File is not exist!\nUsage: %s filename.csv\n",argv[0]);
+    return 1;
+  }
+
+  //Get X,Y board size
+  if (! file.good()){
+    printf("File is empty!\nUsage: %s filename.csv\n",argv[0]);
+    return 1;
+  }
+  getline (file,line);
+  int comas = countCharQuantity(line,',');
+  //For first line we should get x,y pair
+  if(comas != 1){
+    printf("Wrong format of file!\n1.X,Y size of board\n2.X,Y of start\n3.X,Y of end\nXxY numbers declaring data\nUsage: %s filename.csv\n",argv[0]);
+    return 1;
+  }
+  int table[comas+1];
+  LineToIntArray(line,table);
+  if(table[0] < 2 || table[0] > 400 || table[1]<2 || table[1]>400){
+    printf("Table size 2<=x,y<=400");
+    return 1;
+  }
+  int fieldX = table[0];
+  int fieldY = table[1];
+
+  //Get X,Y start point
+  if (! file.good()){
+    printf("Wrong format of file!\nUsage: %s filename.csv\n",argv[0]);
+    return 1;
+  }
+  getline (file,line);
+  comas = countCharQuantity(line,',');
+  //For second line we should get x,y pair
+  if(comas != 1){
+    printf("Wrong format of file!\n1.X,Y size of board\n2.X,Y of start\n3.X,Y of end\nXxY numbers declaring data\nUsage: %s filename.csv\n",argv[0]);
+    return 1;
+  }
+  LineToIntArray(line,table);
+  Point startPoint;
+  startPoint.x = table[0];
+  startPoint.y = table[1];
+
+  //Get X,Y end point
+  if (! file.good()){
+    printf("Wrong format of file!\nUsage: %s filename.csv\n",argv[0]);
+    return 1;
+  }
+  getline (file,line);
+  comas = countCharQuantity(line,',');
+  //For first line we should get x,y pair
+  if(comas != 1){
+    printf("Wrong format of file!\n1.X,Y size of board\n2.X,Y of start\n3.X,Y of end\nXxY numbers declaring data\nUsage: %s filename.csv\n",argv[0]);
+    return 1;
+  }
+  LineToIntArray(line,table);
+  Point endPoint;
+  endPoint.x = table[0];
+  endPoint.y = table[1];
+
+  Point field
+
+  //Raport
+  printf("B: %d x %d\n",fieldX,fieldY);
+  printf("S: %d x %d\n",startPoint.x,startPoint.y);
+  printf("E: %d x %d\n",endPoint.x,endPoint.y);
+
+return 0;
+
+  // while ( file.good() )
+  // {
+  //   getline (file,line);
+  //   std::cout << line << std::endl;
+  // }
+  file.close();
+
+  Point qdw[400][400];
+  qdw[50][50].x = 4;
+
   bool end = false;
   int iteration = 0;
 
@@ -88,7 +240,7 @@ int main()
 
       SDL_RenderPresent(renderer);
 
-      SDL_Delay(1000);
+      // SDL_Delay(500);
       if(iteration >= 2)
         end = true;
     }while(!end); 
